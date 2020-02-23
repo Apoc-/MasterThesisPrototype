e2e160564d512c547de3bc709fc9e07839b984b5
René Ziegler <ziegler.rene87@gmx.de>
2020-02-23 17:59:55 +0100

    Added agile manifesto wiki

---
+++ Assets/Scripts/Interaction/ManifestoWikiInteractible.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+using UI;
+using Random = UnityEngine.Random;
+
+namespace Core
+{
+    public class ManifestoWikiInteractible : Interactible
+    {
+        public WikiScreenBehaviour ManifestoWikiScreen;
+        
+        public override void StartInteraction(Entity entity)
+        {
+            ManifestoWikiScreen.Show();       
+        }
+
+        public override void FinishInteraction(Entity entity)
+        {
+            
+        }
+
+        public override string GetName() => "Das Agile Manifest";
+        public override string GetTooltip() => "Das Agile Manifest lesen";
+    }
+}

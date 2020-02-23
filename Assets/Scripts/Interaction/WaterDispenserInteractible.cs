2b17f13fa374e9e15a4edd44a91c714f3ab1a092
René Ziegler <ziegler.rene87@gmx.de>
2020-02-23 17:30:55 +0100

    Added waterdispenser, move starting points

---
+++ Assets/Scripts/Interaction/WaterDispenserInteractible.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class WaterDispenserInteractible : Fixable
+    {
+        private void Start()
+        {
+            _brokenTooltips = new List<string> {
+                "Becher bereitstellen", "Wassertank austauschen"
+            };
+        }
+
+        public override string GetName() => "Wasserspender";
+
+        public override void OnFixed()
+        {
+            
+        }
+    }
+}

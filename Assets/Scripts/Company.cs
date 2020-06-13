681b989a95eacb8bd9ec97d8b615007f61fea714
René Ziegler <ziegler.rene87@gmx.de>
2020-06-13 13:17:01 +0200

    Fixed errors in phone call texts

--- Assets/Scripts/Company.cs
+++ Assets/Scripts/Company.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.CodeDom;
+﻿using Core;
 using System.Collections.Generic;
 using System.Linq;
-using Core;
 using Tasklist;
-using UI;
 using UnityEngine;
 
 public class Company
